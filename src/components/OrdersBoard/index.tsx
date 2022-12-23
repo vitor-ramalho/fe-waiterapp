87b410fd79d8d69fde453c65b493f93d4776e2e0
{"nickname": "JSON"} <39497088+vitor-ramalho@users.noreply.github.com>
2022-12-22 22:14:41 -0300

    Frontend WaiterAPP

---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Order } from "../../types/Order";
+import { api } from "../../utils/api";
+import { OrderModal } from "../OrderModal";
+import { Board, OrdersContainer } from "./styles";
+import { toast } from "react-toastify";
+
+interface OrdersBoardProps {
+  icon: string;
+  title: string;
+  orders: Order[];
+  onCancelOrder: (orderId: string) => void;
+  onChangeOrderStatus: (orderId: string, status: Order["status"]) => void;
+}
+
+export function OrdersBoard({ icon, title, orders, onCancelOrder, onChangeOrderStatus }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
+  async function handleCancelOrder() {
+    setIsLoading(true);
+    await api.delete(`/order/${selectedOrder?._id}`);
+
+    toast.success(`O pedido da mesa ${selectedOrder?.table} foi cancelado`);
+    onCancelOrder(selectedOrder!._id);
+    setIsLoading(false);
+    setIsModalVisible(false);
+  }
+
+  async function handleChangeOrderStatus() {
+    setIsLoading(true);
+
+    const newStatus = selectedOrder?.status === "WAITING"
+      ? "IN_PRODUCTION"
+      : "DONE";
+
+    await api.patch(`/order/${selectedOrder?._id}`, { status: newStatus });
+
+    toast.success(`O pedido da mesa ${selectedOrder?.table} foi alterado`);
+    onChangeOrderStatus(selectedOrder!._id, newStatus);
+    setIsLoading(false);
+    setIsModalVisible(false);
+  }
+
+  return (
+    <>
+      <Board>
+        <OrderModal
+          visible={isModalVisible}
+          order={selectedOrder}
+          onClose={handleCloseModal}
+          onCancelOrder={handleCancelOrder}
+          isLoading={isLoading}
+          onChangeOrderStatus={handleChangeOrderStatus}
+        />
+        <header>
+          <span>{icon}</span>
+          <strong>{title}</strong>
+          <span>({orders.length})</span>
+        </header>
+
+        {orders.length > 0 &&
+          <OrdersContainer>
+            {orders.map((order) => (
+              <>
+                <button type="button" key={order._id} onClick={() => handleOpenModal(order)}>
+                  <strong>Mesa {order.table}</strong>
+                  <span>{order.products.length}</span>
+                </button>
+              </>
+            ))}
+          </OrdersContainer>
+        }
+      </Board>
+    </>
+  );
+}
